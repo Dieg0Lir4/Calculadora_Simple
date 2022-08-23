ce515b2efb8eb57b4d6051f0eff70167a4232773
Dieg0Lir4 <111295816+Dieg0Lir4@users.noreply.github.com>
2022-08-23 08:05:40 -0500

    Add files via upload

---
+++ avance2Programacion.py
@@ -0,0 +1,8 @@
+num1 = float(input("Ingresa un número: "))
+operador = input("Ingresa un operador: +   -   *   /:   ")
+num2 = float(input("Ingresa otro numero: "))
+
+print("La suma de esos numeros es: ", num1 + num2)
+print("La resta de esos numeros es: ", num1 - num2)
+print("La multiplicacion de esos numeros es: ", num1 * num2)
+print("La division de esos numeros es: ", num1 / num2)
